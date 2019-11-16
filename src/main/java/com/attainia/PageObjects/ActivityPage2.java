6e3d24dc4a09f6454926d8bda6c1d3ec0395d67a
Rajath <rajath.vp@gmail.com>
2019-11-16 15:31:57 -0800

    Update ActivityPage2.java
    added a comment

--- src/main/java/com/attainia/PageObjects/ActivityPage2.java
+++ src/main/java/com/attainia/PageObjects/ActivityPage2.java
@@ -8,5 +8,8 @@ public class ActivityPage2 {
 	// grab the elements of the dynamci table 
 	
 	//WebElement table = 
+	
+	//Since both pages are similar I have avoided duplicating the elements
+
 
 }
